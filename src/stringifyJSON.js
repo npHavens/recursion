4a93a02ae2b32183d346c8657297690e4a14e024
npHavens <npahavens@gmail.com>
2017-05-06 13:27:53 -0500

    Added psuedocode outline

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,19 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //instantiate variable to hold data type of obj
+  //BASE CASE: if data type is string, return obj
+
+  //if data type is number, return obj.toString()
+
+  //if data type is array, iterate over elements of obj
+    //instantiate var stringifiedArray '['
+    //for each element
+      //stringifiedArray.concat(stringifyJSON(element) + ',')
+    //return stringifiedArray.concat(']')
+
+  //else iterate over properties of obj
+    //instantiate var stringifiedObj '{'
+    //for each prop
+      //stringifiedObj.concat(prop + ':' + stringifyJSON(obj[prop]) + ',')
 };
