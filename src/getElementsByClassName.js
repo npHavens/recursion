908d4f12e1c3b1595ab49121ddd6ac9f24e3c066
npHavens <npahavens@gmail.com>
2017-05-07 10:18:53 -0500

    Scrapped previous implementation and adde new pseudocode that uses internal recursive function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,39 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
- 
+var getElementsByClassName = function(className) {
   //instantiate empty array for results
-  var matchedElements = [];
-  //instantiate variable for current element
-  var node = $(this);
-  console.log(node.childNodes)
-  //define function for checking if element has className
-  var hasClass = function(element) {
-   //iterate through class list and push element to matchedElements if there is a match
-    if (element.classList) {
-      for (var i = 0; i < element.classList.length; i++) {
-        if (element.classList[i] === className) {
-    	  return true;
-        }
-      }
-    }
-    return false;
-  }
+  
+  //instantiate var body
 
-  if (hasClass(node)) {
-  	matchedElements.push(node);
-  }
-  
-  //BASE CASE: if node.childNodes.length === 0, return matched elements
-  if (!node.childNodes) {
-  	return matchedElements;
-  }
-  
-  //else iterate over node.childNodes
-  for (var i = 0; i < node.childNodes.length; i++) {
-    matchedElements = matchedElements.concat(getElementsByClassName.call(node.childNodes[i], className))
-  }
- 
+  //define function iterateNodes(node) to iterate over body
+    //var node = current element
+    //if node.classList.contains(className)
+      //results.push(node)
+    //if node.childNodes
+      //iterate over childNodes
+        //for each childNode
+          //iterateNode(childNode)
+    //BASE CASE: else return results
+
+  //iterateNodes(body)
+
+  //return results
 };
